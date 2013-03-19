17bd7fbb1a6b3c9af2c283fbe50da631e6e77b19
Matthew Kirkley <matt.kirkley@gmail.com>
2013-03-19 18:50:57 -0400

    InTheatersAction which hits rottemtomatoes API for theater movies.

---
+++ src/groovy/edu/kirkley/moviereminder/rottentomatoesapi/action/InTheatersAction.groovy
@@ -0,0 +1,48 @@
+package edu.kirkley.moviereminder.rottentomatoesapi.action
+
+import groovy.json.JsonSlurper
+
+class InTheatersAction {
+
+    String baseUrl
+
+    int itemsPerPage
+
+    int pageNumber
+
+    String firstPageUrl
+
+    String apiKey
+
+    InTheatersAction() {}
+
+    InTheatersAction(String baseUrl, String apiKey, int itemsPerPage = 16, int pageNumber = 1) {
+        this.baseUrl = baseUrl
+        this.itemsPerPage = itemsPerPage
+        this.pageNumber = pageNumber
+        this.apiKey = apiKey
+    }
+
+    def getResult() {
+        def currentUrl = getUrl() 
+
+        def json = currentUrl.toURL().getText()
+
+        def jsonMap = new JsonSlurper().parseText(json)  
+
+        //def prevUrl = jsonMap.links.self
+
+        def nextUrl = jsonMap.links.next
+
+        def nextPageTheaterAction
+        if (nextUrl) {
+            nextPageTheaterAction = new InTheatersAction(baseUrl, apiKey, itemsPerPage, pageNumber+1)
+        }
+
+        [json:json,movies:jsonMap,nextPage:nextPageTheaterAction]
+    }
+
+    def getUrl() {
+        "${baseUrl}/lists/movies/in_theaters.json?apikey=${apiKey}&page_limit=${itemsPerPage}&page=${pageNumber}"
+    }
+}
