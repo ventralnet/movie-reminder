6d6c87a49b35b576fb14be2f1f511a92ec7b9d28
Matthew Kirkley <matt.kirkley@gmail.com>
2013-03-19 17:00:06 -0400

    initial job

---
+++ grails-app/jobs/edu/kirkley/moviereminder/job/InTheaterCachePopulateJob.groovy
@@ -0,0 +1,24 @@
+package edu.kirkley.moviereminder.job
+
+class InTheaterCachePopulateJob {
+    static triggers = {
+      simple repeatInterval: 5000l // execute job once in 5 seconds
+    }
+
+    def rottenTomatoesAPIService
+    static onlyOnceHack = 0
+    def execute() {
+        if (onlyOnceHack == 0){
+        def result = rottenTomatoesAPIService.executeInTheaterMovieQuery()
+        def nextUrl = result.nextUrl
+        println nextUrl
+        onlyOnceHack++;
+        while (nextUrl) {
+            nextUrl = rottenTomatoesAPIService.executeInTheaterMovieQuery(nextUrl).nextUrl
+            println nextUrl  
+            Thread.sleep(500)       
+        }
+        }
+        
+    }
+}
---
+++ test/unit/edu/kirkley/moviereminder/service/RottenTomatoesAPIServiceTests.groovy
@@ -0,0 +1,17 @@
+package edu.kirkley.moviereminder.service
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(RottenTomatoesAPIService)
+class RottenTomatoesAPIServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
