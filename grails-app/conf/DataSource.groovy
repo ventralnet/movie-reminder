625b5aa4ac86abaa03db39219e61453a4d2ba639
Matthew Kirkley <matt.kirkley@gmail.com>
2013-03-21 17:03:32 -0400

    Using inmemory database for development

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,7 +1,10 @@
 dataSource {
     pooled = true
-    driverClassName = "com.mysql.jdbc.Driver"
-    dialect =  "org.hibernate.dialect.MySQL5InnoDBDialect"
+    //driverClassName = "com.mysql.jdbc.Driver"
+    //dialect =  "org.hibernate.dialect.MySQL5InnoDBDialect"
+    driverClassName = "org.h2.Driver"
+    username="sa"
+    password=""
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -12,10 +15,11 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://localhost/movie_reminder_dev?useUnicode=yes&characterEncoding=UTF-8"
-            username = "root"
-            password = "secret"
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //url = "jdbc:mysql://localhost/movie_reminder_dev?useUnicode=yes&characterEncoding=UTF-8"
+            //username = "root"
+            //password = "secret"
         }
         hibernate {
             show_sql = true
