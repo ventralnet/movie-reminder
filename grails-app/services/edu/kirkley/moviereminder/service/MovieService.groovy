6d5938dbbad1ca97a92300d65e07c8c618586e35
Matthew Kirkley <matt.kirkley@gmail.com>
2013-03-21 17:44:56 -0400

    line delimiters to unix

--- grails-app/services/edu/kirkley/moviereminder/service/MovieService.groovy
+++ grails-app/services/edu/kirkley/moviereminder/service/MovieService.groovy
@@ -8,24 +8,10 @@ class MovieService {
     
     def apiKey = "a8nefdgjj5b9y482v453vmtn"
     
-    def executeInTheaterMovieQuery(url) {
-        
-        def json
-        if (url) {
-            url += "&apikey=${apiKey}"
-            json = url.toURL().getText()
-        } else {
-            url = "${baseUrl}lists/movies/in_theaters.json?apikey=${apiKey}&page_limit=20&country=us"
-            json = url.toURL().getText()
-        }
-        
-        json = new JsonSlurper().parseText(json)
-        
-        def nextUrl = json.links.next
-        
-        def movies = json.movies
-        
-        [movies:movies,nextUrl:nextUrl]
-                
+    def pageSize = 9
+    
+    def getMoviesInTheaters(pageNumber = 1) {
+        def movies = Movie.findAll
     }
+    
 }
