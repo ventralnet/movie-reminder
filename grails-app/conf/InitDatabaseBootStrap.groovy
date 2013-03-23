bd3731bb9dc28a1109e43aa34b4a5dda09fcd80c
Matthew Kirkley <matt.kirkley@gmail.com>
2013-03-23 17:26:56 -0400

    updated init script to include more sample json files.

--- grails-app/conf/InitDatabaseBootStrap.groovy
+++ grails-app/conf/InitDatabaseBootStrap.groovy
@@ -8,7 +8,7 @@ class InitDatabaseBootStrap {
     def init = { servletContext -> 
         switch(GrailsUtil.environment) {
             case "development":
-                def sampleJsonFilenames = ["in_theater_page1.json", "in_theater_page2.json", "in_theater_page3.json", "in_theater_page4.json", "in_theater_page5.json", "in_theater_page6.json", "in_theater_page7.json", "in_theater_page8.json", "in_theater_page9.json"]
+                def sampleJsonFilenames = getSampleJsonFilenames()
                 sampleJsonFilenames.each { filename ->
                     final URL resource = this.getClass().getClassLoader().getResource("resources/${filename}");
                     def jsonToMovieTransformer = new JsonToMovieTransformer()
@@ -25,4 +25,36 @@ class InitDatabaseBootStrap {
     def destroy = {
     }
 
+    private def getSampleJsonFilenames() {
+        ["box_office1.json",
+        "current_releases1.json",
+        "current_releases2.json",
+        "current_releases3.json",
+        "current_releases4.json",
+        "in_theater_page1.json",
+        "in_theater_page2.json",
+        "in_theater_page3.json",
+        "in_theater_page4.json",
+        "in_theater_page5.json",
+        "in_theater_page6.json",
+        "in_theater_page7.json",
+        "in_theater_page8.json",
+        "in_theater_page9.json",
+        "in_theaters1.json",
+        "in_theaters2.json",
+        "in_theaters3.json",
+        "in_theaters4.json",
+        "in_theaters5.json",
+        "in_theaters6.json",
+        "in_theaters7.json",
+        "in_theaters8.json",
+        "new_releases1.json",
+        "new_releases2.json",
+        "new_releases3.json",
+        "new_releases4.json",
+        "opening1.json",
+        "upcoming1.json",
+        "upcoming2.json",
+        "upcoming3.json"]
+    }
 }
