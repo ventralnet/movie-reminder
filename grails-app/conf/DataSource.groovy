0694ea7327353db8baf9003978ed2f5937cd356b
Matthew Kirkley <matt.kirkley@gmail.com>
2013-04-30 20:39:25 -0400

    Datasource updated.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,10 +1,10 @@
 dataSource {
     pooled = true
-    //driverClassName = "com.mysql.jdbc.Driver"
-    //dialect =  "org.hibernate.dialect.MySQL5InnoDBDialect"
-    driverClassName = "org.h2.Driver"
-    username="sa"
-    password=""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect =  "org.hibernate.dialect.MySQL5InnoDBDialect"
+    //driverClassName = "org.h2.Driver"
+    //username="sa"
+    //password=""
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -16,10 +16,21 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            //url = "jdbc:mysql://localhost/movie_reminder_dev?useUnicode=yes&characterEncoding=UTF-8"
-            //username = "root"
-            //password = "secret"
+            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/movie_reminder_dev?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "secret"
+            pooled = true
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=true
+               validationQuery="SELECT 1"
+            }
         }
         hibernate {
             show_sql = true
